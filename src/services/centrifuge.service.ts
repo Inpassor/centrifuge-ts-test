ebc3d062a3352b1af8bc4c0e5d2564f36ad0b8de
Oleg V. Kuzmin <inpassor@yandex.com>
2018-02-27 23:44:46 +0300

    Work in progress
    Signed-off-by: Oleg V. Kuzmin <inpassor@yandex.com>

--- src/services/centrifuge.service.ts
+++ src/services/centrifuge.service.ts
@@ -157,7 +157,7 @@ export class CentrifugeService {
     }
 
     private _anyToUint8Array(data: any): Uint8Array {
-        data = JSON.stringify(data);
+        data += '';
         const buffer = new Uint8Array(data.length);
         Array.prototype.forEach.call(data, (c, i) => buffer[i] = c.charCodeAt(0));
         return buffer;
