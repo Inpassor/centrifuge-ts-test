258cd33e080cdb92f27615f2556797a808154a76
Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>
2018-03-12 10:09:15 +0300

    Work in progress
    Signed-off-by: Oleg Kuzmin <oleg.kuzmin@arcadia.spb.ru>

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,6 @@
+import {Subject} from 'rxjs/Subject';
 import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 import * as SockJS from 'sockjs-client';
 import {sha256} from 'js-sha256';
@@ -22,60 +24,75 @@ export class IndexComponent {
 
     constructor(private _appService: AppService,
                 private _settingsService: SettingsService,
-                private _centrifugeService: CentrifugeService) {
+                private _centrifugeService: CentrifugeService,
+                private _httpClient: HttpClient) {
         this._appService.title = 'Index';
 
-        const now = +Date.now();
-        const user = 'user_' + now;
-        const exp = '1000000';
-        const infoRaw = {
-            x: 100,
-            y: 100,
-        };
-        let info;
-        let infoSign;
-        if (this._settingsService.isProtobufFormat) {
-            info = infoSign = ctt.ClientInfo.encode(infoRaw).finish();
-        } else {
-            info = infoRaw;
-            infoSign = JSON.stringify(infoRaw);
-        }
-        const sign = this._generateClientSign(user, exp, infoSign);
-
-        this._centrifugeService.connect({
-            format: this._settingsService.format,
-            url: this._settingsService.connectionUrl,
-            user,
-            exp,
-            info,
-            sign,
-            // sockJS: SockJS,
-        }).subscribe(() => {
-            this._centrifugeService.subscribe('system', {
-                message: IndexComponent._handleMessage,
-                presence: (result: proto.PresenceResult) => {
-                    if (result.presence) {
-                        for (const clientUid in result.presence) {
-                            if (result.presence.hasOwnProperty(clientUid)) {
-                                const clientInfo: proto.IClientInfo = result.presence[clientUid];
-                                let connInfo;
-                                if (this._settingsService.isProtobufFormat) {
-                                    connInfo = ctt.ClientInfo.decode(clientInfo.connInfo);
-                                } else {
-                                    connInfo = clientInfo['conn_info'];
+        this._getConnectionParameters().subscribe((connectionParameters: proto.IConnectRequest) => {
+            this._centrifugeService.connect({
+                format: this._settingsService.format,
+                url: this._settingsService.connectionUrl,
+                user: connectionParameters.user,
+                exp: connectionParameters.exp,
+                info: connectionParameters.info,
+                sign: connectionParameters.sign,
+                // sockJS: SockJS,
+            }).subscribe(() => {
+                this._centrifugeService.subscribe('system', {
+                    message: IndexComponent._handleMessage,
+                    presence: (result: proto.PresenceResult) => {
+                        if (result.presence) {
+                            for (const clientUid in result.presence) {
+                                if (result.presence.hasOwnProperty(clientUid)) {
+                                    const clientInfo: proto.IClientInfo = result.presence[clientUid];
+                                    let connInfo;
+                                    if (this._settingsService.isProtobufFormat) {
+                                        connInfo = ctt.ClientInfo.decode(clientInfo.connInfo);
+                                    } else {
+                                        connInfo = clientInfo['conn_info'];
+                                    }
+                                    console.log(connInfo);
                                 }
-                                console.log(connInfo);
                             }
                         }
-                    }
-                },
-            });
-            this._centrifugeService.publish('system', 'TEST!!!').subscribe(() => {
-                LoggerService.debug('Publish success!');
-            }, (err: proto.IError) => {
-                LoggerService.debug('Publish error', err);
+                    },
+                });
+                this._centrifugeService.publish('system', 'TEST!!!').subscribe(() => {
+                    LoggerService.debug('Publish success!');
+                }, (err: proto.IError) => {
+                    LoggerService.debug('Publish error', err);
+                });
             });
         });
+    }
+
+    private _getConnectionParameters(): Subject<proto.IConnectRequest> {
+        const subject = new Subject();
+        if (this._settingsService.backendUrl) {
+            return <Subject<proto.IConnectRequest>>this._httpClient.get(this._settingsService.backendUrl);
+        } else {
+            const infoRaw = {
+                x: 100,
+                y: 100,
+            };
+            const result: proto.IConnectRequest = {
+                user: 'user_' + +Date.now(),
+                exp: '1000000',
+            };
+            let infoSign;
+            if (this._settingsService.isProtobufFormat) {
+                result.info = infoSign = ctt.ClientInfo.encode(infoRaw).finish();
+            } else {
+                result.info = <any>infoRaw;
+                infoSign = JSON.stringify(infoRaw);
+            }
+            result.sign = this._generateClientSign(result.user, result.exp, infoSign);
+            setTimeout(() => {
+                subject.next(result);
+                subject.complete();
+            }, 10);
+        }
+        return subject;
     }
 
     private _generateClientSign(user: string, exp: string | number, info: string | number[] | ArrayBuffer | Uint8Array = ''): string {
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -3,6 +3,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class SettingsService {
 
+    public backendUrl = null;
+    // public backendUrl = 'http://backend.local';
+
     public connectionUrl = 'ws://centrifugo2.local/connection/websocket';
     public secret = '1234567890';
     public format = 'json';
